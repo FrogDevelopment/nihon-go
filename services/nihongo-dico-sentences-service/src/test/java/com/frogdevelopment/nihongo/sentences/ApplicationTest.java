11bfcfb23820213248a6646d7ac3274fdcaebd56
FrogDevelopper <legall.benoit@gmail.com>
2020-12-20 15:21:24 +0100

    fix: tests

--- services/nihongo-dico-sentences-service/src/test/java/com/frogdevelopment/nihongo/sentences/ApplicationTest.java
+++ services/nihongo-dico-sentences-service/src/test/java/com/frogdevelopment/nihongo/sentences/ApplicationTest.java
@@ -1,9 +1,12 @@
 package com.frogdevelopment.nihongo.sentences;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -13,8 +16,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 class ApplicationTest {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        Assertions.assertThat(applicationContext).isNotNull();
     }
 
 }
