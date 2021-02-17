cbbec85de31ac585a1d55abea1e41f5da933d98c
Le Gall Benoit <legall.benoit@gmail.com>
2021-02-17 10:24:33 +0100

    fix: add tests

--- modules/utils-module/src/test/java/com/frogdevelopment/nihongo/export/ExportByLangTest.java
+++ modules/utils-module/src/test/java/com/frogdevelopment/nihongo/export/ExportByLangTest.java
@@ -1,39 +1,78 @@
 package com.frogdevelopment.nihongo.export;
 
+import java.nio.file.Path;
+import java.sql.ResultSet;
+import java.util.Iterator;
+import java.util.List;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+import org.mockito.stubbing.VoidAnswer2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.frogdevelopment.nihongo.multischema.Language.ENG;
+import static org.mockito.AdditionalAnswers.answerVoid;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
 
-@Tag("unitTest")
-@ExtendWith(MockitoExtension.class)
+//@Transactional
+//@SpringBootTest
+//@Tag("integrationTest")
+//@ActiveProfiles("test")
 class ExportByLangTest {
 
-    @InjectMocks
+    @Autowired
     private ExportByLang exportByLang;
-    @Mock
+    @MockBean
     private PathExportManager pathExportManager;
-    @Mock
+    @MockBean
     private ExportDao exportDao;
 
     @Test
-    void shouldExportForGivenLang() {
+    void shouldExportForGivenLang(@TempDir Path tempDir) {
+        // given
+        given(pathExportManager
+                .getPathForLang(ENG.getCode()))
+                .willReturn(tempDir);
+
+        doAnswer(answerVoid((VoidAnswer2<String, ResultSetExtractor<Void>>) (lang, rse) -> {
+            var iterator = List.of(
+                    "{line_1}",
+                    "{line_2}",
+                    "{line_3}",
+                    "{line_4}"
+            ).iterator();
+
+            ResultSet resultSet = mock(ResultSet.class);
+            given(resultSet.getString(1)).willReturn(iterator.next());
+            given(resultSet.next()).willReturn(iterator.hasNext());
+
+            rse.extractData(resultSet);
+        })).when(exportDao).export(anyString(), any());
+
         // when
         exportByLang.call(ENG);
 
         // then
-        then(exportDao)
-                .should()
-                .export(eq(ENG.getCode()), any());
-//        then(pathExportManager)
-//                .should()
-//                .getPathForLang(ENG.getCode());
+
     }
+
+
 }
