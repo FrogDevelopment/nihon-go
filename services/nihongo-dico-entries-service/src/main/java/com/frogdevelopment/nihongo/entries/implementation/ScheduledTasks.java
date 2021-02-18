863e62c3b8388bdc9382438caf35feb05ea390f0
FrogDevelopper <legall.benoit@gmail.com>
2021-02-18 20:52:58 +0100

    feat(wake-up): schedule a task to keep postgres wake-up

---
+++ services/nihongo-dico-entries-service/src/main/java/com/frogdevelopment/nihongo/entries/implementation/ScheduledTasks.java
@@ -0,0 +1,23 @@
+package com.frogdevelopment.nihongo.entries.implementation;
+
+import com.frogdevelopment.nihongo.entries.implementation.search.Search;
+import com.frogdevelopment.nihongo.multischema.Language;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ScheduledTasks {
+
+    private final Search search;
+
+    @Scheduled(cron = "0 * * * * *")
+    public void wakeUpPostgres() {
+        log.info("Scheduled task wakeUpPostgres");
+        search.call(Language.ENG.getCode(), "eat");
+    }
+}
+
