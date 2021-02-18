5d5a068f3e5ff7fc06a387d83a7d06f99cbb5051
Le Gall, Benoit <benoit.legall@rakuten.com>
2021-02-18 13:59:38 +0100

    fix(export): fix and add tests

--- services/nihongo-dico-sentences-service/src/test/java/com/frogdevelopment/nihongo/sentences/implementation/populate/FetchSentencesTest.java
+++ services/nihongo-dico-sentences-service/src/test/java/com/frogdevelopment/nihongo/sentences/implementation/populate/FetchSentencesTest.java
@@ -1,6 +1,9 @@
 package com.frogdevelopment.nihongo.sentences.implementation.populate;
 
-import com.frogdevelopment.nihongo.sentences.implementation.about.AboutDao;
+import static org.mockito.BDDMockito.then;
+
+import java.util.Map;
+
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,9 +11,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Map;
-
-import static org.mockito.BDDMockito.then;
+import com.frogdevelopment.nihongo.export.ExportData;
+import com.frogdevelopment.nihongo.sentences.implementation.about.AboutDao;
 
 @Tag("unitTest")
 @ExtendWith(MockitoExtension.class)
@@ -25,6 +27,8 @@ class FetchSentencesTest {
     private AboutDao aboutDao;
     @Mock
     private DeleteDownloadedFiles deleteDownloadedFiles;
+    @Mock
+    private ExportData exportData;
 
     @Test
     void shouldCallFetcher() {
@@ -35,6 +39,7 @@ class FetchSentencesTest {
         then(populateDatabase).should().call();
         then(aboutDao).should().generate(Map.of());
         then(deleteDownloadedFiles).should().call();
+        then(exportData).should().call();
     }
 
 }
