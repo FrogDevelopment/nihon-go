a10474e0d02ce81af433c629a502b74f056845f2
Le Gall Benoit <legall.benoit@gmail.com>
2021-12-26 21:20:16 +0100

    fat(dependencies): various update

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,7 @@ plugins {
     id("nihongo.version-convention")
 }
 
-
 tasks.wrapper {
-    gradleVersion = "7.3.1"
+    gradleVersion = "7.3.3"
     distributionType = Wrapper.DistributionType.ALL
 }
--- buildSrc/src/main/kotlin/nihongo.base-convention.gradle.kts
+++ buildSrc/src/main/kotlin/nihongo.base-convention.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 
 dependencyManagement {
     imports {
-        mavenBom("org.junit:junit-bom:5.8.1")
+        mavenBom("org.junit:junit-bom:5.8.2")
         mavenBom("org.testcontainers:testcontainers-bom:1.16.2")
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:2021.0.0")
     }
