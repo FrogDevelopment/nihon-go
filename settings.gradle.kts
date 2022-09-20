0382620738c85e25a02be7abfe1d622711874134
Le Gall, Benoit <legall.benoit@gmail.com>
2022-09-20 12:10:18 +0200

    fix npm builds

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,12 +7,12 @@ include(
     ":services:nihongo-dico-entries-service",
     ":services:nihongo-dico-sentences-service",
     ":services:nihongo-lessons-service",
-//        ":web:frog-manager-web",
-//        ":web:nihongo-dico-web"
+    ":web:frog-manager-web",
+    ":web:nihongo-dico-web"
 )
 
-//project(":front:frog-manager-web").name = "nihongo-dico"
-//project(":front:nihongo-dico-web").name = "nihongo-admin"
+project(":web:frog-manager-web").name = "nihongo-dico"
+project(":web:nihongo-dico-web").name = "nihongo-admin"
 
 
 dependencyResolutionManagement {
